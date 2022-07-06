dc25d265528d468d9d268c1df02c591491a360ab
Paulo Sergio <pauloserg861@gmail.com>
2022-07-06 14:40:39 -0300

    estrutura inicial de projeto

---
+++ src/pages/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    Pagina About
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    Pagina Home
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
