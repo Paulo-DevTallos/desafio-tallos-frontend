8f47fc99bb4c9c353dea91867087b2e8d92b4481
Paulo Sergio <pauloserg861@gmail.com>
2022-07-08 13:37:33 -0300

    criando header home-page

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,15 +1,23 @@
 <template>
-  <main>
-    Pagina Home
-    novo teste de pagina
-    <RouterLink to="/login">
-      to login
-    </RouterLink>
-  </main>
+  <div>
+    <Header />
+      <main>
+        Pagina Home
+        novo teste de pagina novo teste
+        <RouterLink to="/login">
+          to login
+        </RouterLink>
+      </main>
+  </div>
 </template>
 
 <script>
+import Header from '../components/Header.vue';
+
 export default {
-  name: 'Home',
+  name: "Home",
+  components: { 
+    Header 
+  },
 }
 </script>
