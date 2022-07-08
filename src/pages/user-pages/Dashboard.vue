272465a9c65b8e199e369920b0dd1090b857674c
Paulo Sergio <pauloserg861@gmail.com>
2022-07-08 15:05:55 -0300

    criando estrutura do Footer de usuario

--- src/pages/user-pages/Dashboard.vue
+++ src/pages/user-pages/Dashboard.vue
@@ -4,18 +4,21 @@
       <Grettings />
       conteudo
     </main>
+  <FooterUser />
 </template>
 
 <script>
 import HeaderUser from '../../components/components-users/HeaderUser.vue';
 import Grettings from '../../components/components-users/Grettings.vue';
+import FooterUser from '../../components/components-users/FooterUser.vue';
 
 export default {
   name: "Dashboard",
   components: {
     HeaderUser,
-    Grettings
-}
+    Grettings,
+    FooterUser,
+  }
 }
 </script>
 
