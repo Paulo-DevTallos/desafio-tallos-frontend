519e10c7238480661a60c3bd874cf66f6d963f40
Paulo Sergio <pauloserg861@gmail.com>
2022-07-11 08:06:57 -0300

    implementando relogio ao footer de usuario

--- src/components/components-users/FooterUser.vue
+++ src/components/components-users/FooterUser.vue
@@ -1,12 +1,27 @@
 <template>
   <footer class="footer-user">
-    Usuário <strong> email@email.com </strong> logado - <span>15:06:00</span>
+    Usuário <strong> email@email.com </strong> logado <span> - {{ data_time }}</span>
   </footer>
 </template>
 
 <script>
 export default {
   name: 'FooterUser',
+  data() {
+    return {
+      data_time: '',
+    }
+  },
+  methods: {
+    printCurrentTime() {
+      return new Date().toLocaleTimeString()
+    }
+  },
+  mounted: function() {
+    setInterval(() => {
+      this.data_time = this.printCurrentTime()
+    }, 1000)
+  }
 }
 </script>
 
