ddcaec4e709b7d1f6f810e23c799614e53cf54c2
Paulo Sergio <pauloserg861@gmail.com>
2022-07-13 16:38:59 -0300

    criando modal delete funcionarios e implementações

---
+++ src/components/components-users/ConfirmModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="confirm-modal">
+    <font-awesome-icon :icon="['fas', 'triangle-exclamation']" />
+    <span>Deseja realmente excluir o usuário?</span>
+    <div class="btn-actions">
+      <button @click="$emit('delete-user')">Confirmar</button>
+      <button @click="$emit('close-modal')">Cancelar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ConfirmModal',
+  emits: ['delete-user', 'close-modal'],
+}
+</script>
+
+<style scoped>
+  @import '../../assets/components/confirm-modal.css';
+</style>
