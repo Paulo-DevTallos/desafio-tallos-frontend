e393c35a5d3da4b947094e4d7990ea107294d4e8
Paulo Sergio <pauloserg861@gmail.com>
2022-07-15 09:12:58 -0300

    ajustando renderização de lista pós exclusão

--- src/pages/user-pages/Chat.vue
+++ src/pages/user-pages/Chat.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    chat
-  </div>
+  <HeaderUser />
+    <main>
+      chat
+    </main>
+  <FooterUser />
 </template>
 
 <script>
+import HeaderUser from '../../components/components-users/HeaderUser.vue'
+import FooterUser from '../../components/components-users/FooterUser.vue'
+
 export default {
   name: 'Chat',
+  components: {
+    HeaderUser,
+    FooterUser
+  }
 }
 </script>
