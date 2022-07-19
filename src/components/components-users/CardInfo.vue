873e33a885edc842a9c720bf73e114ecf5622150
Paulo Sergio <pauloserg861@gmail.com>
2022-07-19 17:01:38 -0300

    configurando card-info

--- src/components/components-users/CardInfo.vue
+++ src/components/components-users/CardInfo.vue
@@ -6,7 +6,10 @@
     <div class="card-id">
       {{ $store.state.user.rules }}
     </div>
-    <div id="logout" class="card-id" @click="logout">
+    <div class="card-id" id="click" @click="updateUser">
+      Atualizar cadastro
+    </div>
+    <div id="click" class="card-id" @click="logout">
       Logout
       <font-awesome-icon :icon="['fas', 'right-from-bracket']" />
     </div>
