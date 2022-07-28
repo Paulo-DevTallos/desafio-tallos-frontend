5cced71882ab7baa8dcd6dc7749100fafb27b80a
Paulo Sergio <pauloserg861@gmail.com>
2022-07-28 10:58:11 -0300

    ajustando estrutura do App

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,8 @@
 </template>
 
 <script>
-import SocketModules from './services/socket'
-
 export default {
   name: 'App',
-  created() {
-    SocketModules.setupSocketConncection()
-  }
 }
 </script>
 
