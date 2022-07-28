f348c7e430167c4a03ed626f6a595e9140c1e477
Paulo Sergio <pauloserg861@gmail.com>
2022-07-28 11:12:53 -0300

    refatorando card info

--- src/components/components-users/CardInfo.vue
+++ src/components/components-users/CardInfo.vue
@@ -6,9 +6,6 @@
     <div class="card-id">
       {{ $store.state.user.rules }}
     </div>
-    <div class="card-id" id="click" @click="updateUser">
-      Atualizar cadastro
-    </div>
     <div id="click" class="card-id" @click="logout">
       Logout
       <font-awesome-icon :icon="['fas', 'right-from-bracket']" />
