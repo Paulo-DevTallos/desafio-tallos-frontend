df07ef22b3c251b57368e2a6afaec92f2c612122
Paulo Sergio <pauloserg861@gmail.com>
2022-07-28 13:37:04 -0300

    criando popups personalizados

---
+++ src/components/alert-popups/PopUpOk.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="box-modal">
+    prop
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PopUpAlert',
+  props: {
+    info_alert: String,
+  }
+}
+</script>
+
+<style scoped>
+  #box-modal {
+    padding: 30px;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    border: 2px solid red;
+    background-color: #ff000068;
+    position: absolute;
+    top: 25px;
+    right: 85px;
+    font-size: 15px;
+    animation: roar .5s;
+  }
+
+  @keyframes roar {
+    from {
+      opacity: 0;
+      visibility: hidden;
+    }
+    to {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+</style>
