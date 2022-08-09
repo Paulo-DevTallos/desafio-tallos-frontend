93c9cc1da2a40ec4088dfad0e8914af86ff6c6e5
Paulo Sergio <pauloserg861@gmail.com>
2022-08-09 13:25:23 -0300

    removendo relogio do footer

--- src/components/components-users/FooterUser.vue
+++ src/components/components-users/FooterUser.vue
@@ -1,27 +1,12 @@
 <template>
   <footer class="footer-user">
-    Usuário <strong> {{ $store.state.user.email }} </strong> logado <span> - {{ data_time }}</span>
+    Usuário <strong> {{ $store.state.user.email }} </strong> logado
   </footer>
 </template>
 
 <script>
 export default {
   name: 'FooterUser',
-  data() {
-    return {
-      data_time: '',
-    }
-  },
-  methods: {
-    printCurrentTime() {
-      return new Date().toLocaleTimeString()
-    }
-  },
-  mounted: function() {
-    setInterval(() => {
-      this.data_time = this.printCurrentTime()
-    }, 1000)
-  }
 }
 </script>
 
