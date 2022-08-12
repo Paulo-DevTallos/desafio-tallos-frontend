7b6d225fab39f418664638ea1a076cfc8fe64cad
Paulo Sergio <pauloserg861@gmail.com>
2022-08-12 11:24:54 -0300

    reposicionando evento de login simultaneo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,49 @@
 <template>
   <RouterView />
+  <PopUpWarn 
+    :info_message="message"
+    v-if="hiddenPopup"  
+  />
 </template>
 
 <script>
+import { io } from 'socket.io-client';
+import PopUpWarn from './components/alert-popups/PopUpWarn.vue';
+const socket = io('http://localhost:3002')
 export default {
   name: 'App',
+  components: {
+    PopUpWarn,
+  },
+  data() {
+    return {
+      message: '',
+      hiddenPopup: false,
+    }
+  },
+  methods: {
+    popupTimeout(msg) {
+      this.hiddenPopup = true
+      this.message = msg
+      setTimeout(() => {
+        this.hiddenPopup = false
+      }, 5000)
+    },
+  },
+  async mounted() {
+    await socket.on('is-logged', (id) => {
+      const sessionLogin = localStorage.getItem('session_id')
+      const currentUserId = id._id.user._id
+
+      if (currentUserId === sessionLogin) {
+        this.popupTimeout(`Você será deslogado devido a um novo acesso`)
+        setTimeout(() => {
+          localStorage.clear()
+          this.$router.push('/login')
+        }, 5000)
+      }
+    })
+  }
 }
 </script>
 
--- src/pages/user-pages/Dashboard.vue
+++ src/pages/user-pages/Dashboard.vue
@@ -3,23 +3,17 @@
     <Template>
       <Grettings />
       <NavMenuDash />
-      <PopUpWarn 
-        :info_message="message"
-        v-if="hiddenPopup"  
-      />
+      
     </Template>
   <FooterUser />
 </template>
 
 <script>
-import { io } from 'socket.io-client';
-const socket = io('http://localhost:3002')
 import HeaderUser from '../../components/components-users/HeaderUser.vue';
 import Grettings from '../../components/components-users/Grettings.vue';
 import FooterUser from '../../components/components-users/FooterUser.vue';
 import NavMenuDash from '../../components/components-users/NavMenuDash.vue';
 import Template from '../../components/components-users/Template.vue';
-import PopUpWarn from '../../components/alert-popups/PopUpWarn.vue';
 
 export default {
   name: "Dashboard",
@@ -29,37 +23,7 @@ export default {
     FooterUser,
     NavMenuDash,
     Template,
-    PopUpWarn,
-  },
-  data() {
-    return {
-      message: '',
-      hiddenPopup: false,
-    }
-  },
-  methods: {
-    popupTimeout(msg) {
-      this.hiddenPopup = true
-      this.message = msg
-      setTimeout(() => {
-        this.hiddenPopup = false
-      }, 5000)
-    },
-  },
-  async mounted() {
-    await socket.on('is-logged', (id) => {
-      const sessionLogin = localStorage.getItem('session_id')
-      const currentUserId = id._id.user._id
-
-      if (currentUserId === sessionLogin) {
-        this.popupTimeout(`Você será deslogado devido a um novo acesso`)
-        setTimeout(() => {
-          localStorage.clear()
-          this.$router.push('/login')
-        }, 5000)
-      }
   
-    })
-  }
+  },
 }
 </script>
