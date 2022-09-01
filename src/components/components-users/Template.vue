99b99fe4076c2e28f5c6e12b86106e67e1862b5a
Paulo Sergio <pauloserg861@gmail.com>
2022-09-01 14:15:16 -0300

    readicionando estrutura

--- src/components/components-users/Template.vue
+++ src/components/components-users/Template.vue
@@ -14,9 +14,9 @@ export default {
   .main-container {
     max-width: 1400px;
     margin: 0 auto;
-    padding: 10px 150px;
+    padding: 10px 100px;
     min-height: 78vh;
-    background-color: #c4c4c42d;
+    background-color: var(--primary-main-bg);
   }
 
   @media(max-width: 1024px) {
@@ -27,7 +27,7 @@ export default {
 
   @media(max-width: 768px) {
     .main-container {
-      padding: 10px; 
+      padding: 0; 
     }
   }
 </style>
