d88a16c86842b51d509efa9098a1c1b63c73dfe3
Paulo Sergio <pauloserg861@gmail.com>
2022-11-29 08:55:47 -0300

    criando componente de input

---
+++ src/components/Inputs/BaseInput.vue
@@ -0,0 +1,37 @@
+<template>
+	<input
+		v-bind="$attrs"
+		v-model="inputValue"
+		@blur="handleValue"
+	/>
+</template>
+
+<script>
+export default {
+	name: 'BaseInput',
+	emits: ['handleValue'],
+	props: {
+		currentValue: {
+			type: String,
+		},
+	},
+
+	data() {
+		return {
+			inputValue: '',
+		}
+	},
+
+	methods: {
+		handleValue(newValue) {
+			this.$emit('handleValue', newValue)
+		}
+	},
+
+	watch: {
+		currentValue(value) {
+			this.inputValue = value
+		}
+	}
+}
+</script>
