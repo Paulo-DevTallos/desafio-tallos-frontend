0f2fc71f9f87ec5b158a1b0475edf55057dadef0
Paulo Sergio <pauloserg861@gmail.com>
2022-11-29 09:15:07 -0300

    criando componente de botao / implementan do formulario

---
+++ src/components/Buttons/MainButton.vue
@@ -0,0 +1,14 @@
+<template>
+	<button class="btn">{{ title_btn }}</button>
+</template>
+
+<script>
+export default {
+	name: 'MainButton',
+	props: {
+		title_btn: {
+			type: String,
+		},
+	},
+}
+</script>
--- src/components/Forms/FormLogin.vue
+++ src/components/Forms/FormLogin.vue
@@ -1,18 +1,30 @@
 <template>
-	<form>
+	<form @submit.prevent="$emit('handleLogin', user)">
 		<div>
-			<BaseInput />
-			<BaseInput />
+			<BaseInput
+				type="text"
+				v-model="user.email"
+				placeholder="Digite seu e-mail"
+			/>
+			<BaseInput
+				type="password"
+				v-model="user.password"
+				placeholder="Digite sua senha"
+			/>
+		</div>
+		<div>
+			<MainButton :title_btn="'Login'" />
 		</div>
 	</form>
 </template>
 
 <script>
+import MainButton from "../Buttons/MainButton.vue";
 import BaseInput from "../Inputs/BaseInput.vue";
 
 export default {
 	name: "FormLogin",
-	components: { BaseInput },
+	components: { BaseInput, MainButton },
 	data() {
 		return {
 			user: {
