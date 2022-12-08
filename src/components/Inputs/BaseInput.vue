9010086f69c8eb1ac0f9f1fbbde75c4188a6561e
Paulo Sergio <pauloserg861@gmail.com>
2022-12-08 11:16:48 -0300

    implementando revelador de senha

--- src/components/Inputs/BaseInput.vue
+++ src/components/Inputs/BaseInput.vue
@@ -1,39 +1,39 @@
 <template>
 	<div class="base-input">
-		<input
-			v-bind="$attrs"
-			v-model="inputValue"
-			@blur="handleValue"
-		/>
+		<input v-bind="$attrs" v-model="inputValue" @blur="handleValue" />
+		<RevellingPassword v-if="hiddenRevellingPassword" />
 	</div>
 </template>
 
 <script>
+import RevellingPassword from "../Icons/RevellingPassword.vue";
+
 export default {
-	name: 'BaseInput',
-	emits: ['handleValue'],
+	name: "BaseInput",
+	emits: ["handleValue"],
 	props: {
 		currentValue: {
 			type: String,
 		},
+		hiddenRevellingPassword: {
+			type: Boolean
+		},
 	},
-
 	data() {
 		return {
-			inputValue: '',
-		}
+			inputValue: "",
+		};
 	},
-
 	methods: {
 		handleValue(newValue) {
-			this.$emit('handleValue', newValue)
-		}
+			this.$emit("handleValue", newValue);
+		},
 	},
-
 	watch: {
 		currentValue(value) {
-			this.inputValue = value
-		}
-	}
-}
+			this.inputValue = value;
+		},
+	},
+	components: { RevellingPassword },
+};
 </script>
