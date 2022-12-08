7ac85c397e6946e56f92fa087f5c739387008c00
Paulo Sergio <pauloserg861@gmail.com>
2022-12-08 16:12:41 -0300

    criando pagina de erro

---
+++ src/pages/NotFoundPage/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="page-error-container">
+		<div class="content-error">
+			<img src="/img/image-tallos-error.png" alt="Imagem de não encontrado">
+			<h3>A página que você está tentando acessar não existe!</h3>
+			<img class="logo" src="/img/tallos-logo.png" alt="logo tallos">
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NotFoundPage',
+}
+</script>
